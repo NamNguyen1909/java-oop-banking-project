f575f167cf1bc04a75b7fea57915d932c9f4a9be
NamNguyen1909 <nguyenthanhnam19092004@gmail.com>
2023-12-25 19:49:40 +0700

    test

---
+++ java_oop_banking_project/src/main/java/com/team/java_oop_banking_project/Java_oop_banking_project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.team.java_oop_banking_project;
+
+/**
+ *
+ * @author THANH_NAM
+ */
+public class Java_oop_banking_project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Github!");
+    }
+}
